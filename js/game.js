92d2079bc398ae3402ce002d4eb443c13fb7500b
patryk@ykka.co <patryk@ykka.co>
2013-11-23 13:24:06 +0000

    Added Score label and Level label blindly, haven't seen it working yet

--- js/game.js
+++ js/game.js
@@ -2,47 +2,50 @@
 /* Game namespace */
 var game = {
 
-	// an object where to store game information
-	data : {
-		// score
-		score : 0
-	},
-	
-	
-	// Run on page load.
-	"onload" : function () {
-	// Initialize the video.
-	if (!me.video.init("screen", 480, 320, true, 'auto')) {
-		alert("Your browser does not support HTML5 canvas.");
-		return;
-	}
+    // an object where to store game information
+    data : {
+        // score
+        score : 0
+    },
 
-	// add "#debug" to the URL to enable the debug Panel
-	if (document.location.hash === "#debug") {
-		window.onReady(function () {
-			me.plugin.register.defer(debugPanel, "debug");
-		});
-	}
 
-	// Initialize the audio.
-	me.audio.init("mp3,ogg");
+    // Run on page load.
+    "onload" : function () {
+    // Initialize the video.
+    if (!me.video.init("screen", 720, 576, true)) {
+        alert("Your browser does not support HTML5 canvas.");
+        return;
+    }
 
-	// Set a callback to run when loading is complete.
-	me.loader.onload = this.loaded.bind(this);
+    // add "#debug" to the URL to enable the debug Panel
+    if (document.location.hash === "") {
+        window.onReady(function () {
+            me.plugin.register.defer(debugPanel, "debug");
+        });
+    }
 
-	// Load the resources.
-	me.loader.preload(game.resources);
+    // Initialize the audio.
+    me.audio.init("mp3,ogg");
 
-	// Initialize melonJS and display a loading screen.
-	me.state.change(me.state.LOADING);
+    // Set a callback to run when loading is complete.
+    me.loader.onload = this.loaded.bind(this);
+
+    // Load the resources.
+    me.loader.preload(game.resources);
+
+    // Initialize melonJS and display a loading screen.
+    me.state.change(me.state.LOADING);
 },
 
-	// Run on game resources loaded.
-	"loaded" : function () {
-		me.state.set(me.state.MENU, new game.TitleScreen());
-		me.state.set(me.state.PLAY, new game.PlayScreen());
+    // Run on game resources loaded.
+    "loaded" : function () {
+        me.state.set(me.state.MENU, new game.TitleScreen());
+        // me.state.set(me.state.PLAY, new game.PlayScreen());
+        me.state.set(me.state.PLAY, new game.VAPlayScreen());
 
-		// Start the game.
-		me.state.change(me.state.MENU);
-	}
+
+        // Start the game.
+        me.state.change(me.state.MENU);
+        console.log('Welcome to V&A Game Jam');
+    }
 };
